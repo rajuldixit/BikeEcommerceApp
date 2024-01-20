14c64335a7684c7adcf19c76d653caf99b463588
rajul786dixit <rajul786dixit@gmail.com>
2024-01-20 23:53:50 +0530

    commentted out code removed

--- src/components/OrdersTable/index.tsx
+++ src/components/OrdersTable/index.tsx
@@ -14,7 +14,6 @@ import React, { useEffect, useState } from "react";
 import { IOrder } from "../../context/AppReducer";
 import { useAppState } from "../../context/AppContext";
 import { IProduct } from "../../utils/types";
-import { IPVersion } from "net";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -30,7 +29,6 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover
   },
-  // hide last border
   "&:last-child td, &:last-child th": {
     border: 0
   }
@@ -44,13 +42,6 @@ function createData(
   return { orderId, customerName, paymentMode, products };
 }
 
-// const rows = [
-//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-//   createData("Eclair", 262, 16.0, 24, 6.0),
-//   createData("Cupcake", 305, 3.7, 67, 4.3),
-//   createData("Gingerbread", 356, 16.0, 49, 3.9)
-// ];
 interface IOrderRow {
   orderId: number;
   customerName: string;
@@ -62,7 +53,6 @@ const OrdersTable = () => {
   const [orderRows, setOrderRows] = useState<Array<IOrderRow>>(new Array());
 
   useEffect(() => {
-    console.log(appState.orders);
     appState.orders.map((order: IOrder) => {
       const row = createData(
         order.orderId,
